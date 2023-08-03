cd3199f945c555cf19f0720618298a7108dfbc9c
aniketn <aniketn@umich.edu>
2023-08-03 18:19:57 -0400

    textbox component

---
+++ src/components/textbox/textbox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Textbox() {
+    return (
+        <div className="overflow-hidden bg-[#f2f2f2] flex flex-col justify-center pl-5 w-full h-12 items-start rounded-lg">
+        <div className="text-xl font-['Poppins'] font-medium capitalize text-[#d9d9d9] w-16">
+            Email
+        </div>
+        </div>
+    );
+}
+
+
+export default Textbox;
