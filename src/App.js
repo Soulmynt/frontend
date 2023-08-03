3eba7aa07c560d7386b2a967eaf900c236634847
Ian Elliott <ie2173@columbia.edu>
2023-08-03 15:35:01 -0700

    did something, I dunno.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Textbox } from "./components/textbox";
 
 function App() {
-  return <Textbox />;
+  return <Textbox text={"I think this is cool"} />;
 }
 
 export default App;
