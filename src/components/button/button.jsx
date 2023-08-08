d1dc52b2a9225095cc161222395651466891a073
Ian Elliott <ie2173@columbia.edu>
2023-08-08 09:07:09 -0700

    button disable defaults to false

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,32 +1,35 @@
 import React from "react";
-import "./button.css"
+import "./button.css";
 
-const Button = ({text, containerWidth, variant, onClick, disabled}) => {
+const Button = ({
+  text,
+  containerWidth,
+  variant,
+  onClick,
+  disabled = false,
+}) => {
   const getButtonClass = () => {
-    if (disabled) return 'disabled-button';
+    if (disabled) return "disabled-button";
 
-    switch(variant) {
-      case 'gray':
-        return 'gray-button';
-      case 'green':
-        return 'green-button';
+    switch (variant) {
+      case "gray":
+        return "gray-button";
+      case "green":
+        return "green-button";
       default:
-        return 'colorful-button';
+        return "colorful-button";
     }
   };
 
   return (
-    <div 
-        className={`button-container ${getButtonClass()}`} 
-        style={{width: containerWidth}}
-        onClick={disabled ? null : onClick} 
+    <div
+      className={`button-container ${getButtonClass()}`}
+      style={{ width: containerWidth }}
+      onClick={disabled ? null : onClick}
     >
-        <div className="button-text">
-            {text}
-        </div>
+      <div className="button-text">{text}</div>
     </div>
   );
 };
-
 
 export default Button;
