5ad0443bfc2c4998e92ed5d23bdaf0389e88085f
aniketn <aniketn@umich.edu>
2023-08-13 14:09:42 -0400

    extra stuff

--- src/pages/signIn/register.jsx
+++ src/pages/signIn/register.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import SignIn from './signIn';
 import SignInStep2 from './signInStep2'
+import SignInStep3 from './signInStep3'
 
 function Register() {
-    const [currentStep, setCurrentStep] = useState(2);
+    const [currentStep, setCurrentStep] = useState(3);
 
     return (
         <div>
             {currentStep === 1 && <SignIn goToNextStep={() => setCurrentStep(2)} />}
             {currentStep === 2 && <SignInStep2 goToNextStep={() => setCurrentStep(3)} />}
-            {/* ... other steps */}
+            {currentStep === 3 && <SignInStep3/>}
         </div>
     );
 }
