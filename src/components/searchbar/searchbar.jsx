5589cc71948f10b6db145cb7698bd86874b55e8d
aniketn <aniketn@umich.edu>
2023-10-02 17:00:10 -0400

    saerchbar

---
+++ src/components/searchbar/searchbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./searchbar.module.css"
+import { Card } from "../card";
+import { SearchIcon } from "../../icons/SearchIcon";
+
+const Searchbar = () => {
+  return( 
+    <div>
+        <Card>
+            
+        
+        </Card>
+
+
+    </div>
+  
+  );
+};
+
+
+export default Searchbar;
