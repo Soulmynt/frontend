d829957e0d62145d3a58b69690e3d95fbfaaa5b4
Ian Elliott <ie2173@columbia.edu>
2023-10-02 13:40:59 -0700

    added AuthRouting

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ import { MyGroups } from "./pages/mygroups";
 import { Explore } from "./pages/explore";
 import { Profile } from "./pages/profile";
 import { Settings } from "./pages/settings";
+import { RequireAuth } from "./components/AuthRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/*  Public Routes */}
         <Route path="/register" element={<Register />} />
+
+        {/* Protected Routes*/}
+        {/*<Route element={<RequireAuth />}>*/}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/create" element={<Create />} />
         <Route path="/mygroups" element={<MyGroups />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/settings" element={<Settings />} />
+        {/* </Route>}*/}
         {/* ... other routes */}
         <Route path="/staging" element={<Staging />} />
       </Routes>
---
+++ src/components/AuthRoute/AuthRoute.jsx
@@ -0,0 +1,13 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../hooks";
+
+const { auth } = useAuth();
+const location = useLocation();
+
+return auth?.user ? (
+  <Outlet />
+) : (
+  <Navigate to="/register" state={{ from: location }} replace />
+);
+
+export default RequireAuth;
