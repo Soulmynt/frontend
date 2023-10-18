b3c074c2794b725a150d2f87b544c6f65cc3d878
aniketn <aniketn@umich.edu>
2023-10-18 16:25:19 -0400

    added login axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -19,4 +19,14 @@ export async function axiosSignIn(email, handle, password, keyGen) {
 
 export async function axiosLogIn(email, password) {
   // Create a Try Catch where we will try to get an object returned from an axios post to our backend's login route.  We will send a req object containing the user's email and password we gathered from the component's state.  if the connection is successful, we will return the res object returned.  Then we will create a catch which will return the error encountered.
+  try {
+    let data = await axios.post(`http://localhost:4000/signin`, {
+      email: email,
+      password: password,
+    });
+    return data;
+  } catch (e) {
+    const errorlog = { data: { error: e } };
+    return errorlog;
+  }
 }
