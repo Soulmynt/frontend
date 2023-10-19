1292d32503e9cb377ab93f9a27b972ee2e7a0197
Ian Elliott <ie2173@columbia.edu>
2023-10-19 15:40:05 -0700

    implemented handlesubmit to login with backend

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -9,114 +9,124 @@ import bottom from "../../img/bottom.png";
 import styles from "./login.module.css";
 import "../../components/textbox/textbox.css";
 import { useState } from "react";
+import { axiosLogIn } from "../../utils/axios";
+import { useAuth } from "../../hooks";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
 
-    const handlePasswordChange = (e) => {
+  const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword);
-    };
+  };
 
-    const handleEmailChange = (e) => {
-        const emailValue = e.target.value;
-        setEmail(emailValue);
-    };
+  const handleEmailChange = (e) => {
+    const emailValue = e.target.value;
+    setEmail(emailValue);
+  };
 
-    const [isPassFocused, setisPassFocused] = useState(false);
+  const handleSubmit = async () => {
+    let data = await axiosLogIn(email, password);
+    setPassword("");
+    setEmail("");
+    if (data.status == 200) {
+      setAuth(data.data.userprofile);
+      navigate("/dashboard");
+    } else {
+      console.log("an error occurred, no workie");
+    }
+  };
 
-    function handleInputFocus() {
-        setisPassFocused(true);
-      }
-    
-      function handleInputBlur() {
-        setisPassFocused(false);
-      }
+  const [isPassFocused, setisPassFocused] = useState(false);
 
+  function handleInputFocus() {
+    setisPassFocused(true);
+  }
 
-    return (
+  function handleInputBlur() {
+    setisPassFocused(false);
+  }
+
+  return (
     <div>
-        <div className={styles.overallSignIn}>
+      <div className={styles.overallSignIn}>
         <div className={styles.leftFrame}>
-            <div className={styles.headerContainer}>
+          <div className={styles.headerContainer}>
             <img src={logo} alt="Description" className={styles.soulmyntLogo} />{" "}
             {/* Add this line */}
             <div className={styles.boldTextContainer}>
-                <div>
+              <div>
                 <BoldText
-                    text={"Soulmynt"}
-                    containerWidth={"130px"}
-                    size={"26px"}
+                  text={"Soulmynt"}
+                  containerWidth={"130px"}
+                  size={"26px"}
                 />
-                </div>
+              </div>
             </div>
             <div className={styles.createAccountContainer}>
-                <div>
-                <ColorfulText
-                    text={"Login"}
-                    containerWidth={"340px"}
-                />
-                </div>
+              <div>
+                <ColorfulText text={"Login"} containerWidth={"340px"} />
+              </div>
             </div>
-            </div>
+          </div>
 
-            <div className={styles.emailPass}>
+          <div className={styles.emailPass}>
             <div>
-                <Textbox
+              <Textbox
                 text={"Email"}
                 type="email"
                 onChange={handleEmailChange}
                 containerWidth={"378px"}
-                />
+              />
             </div>
 
             <div>
-                <Textbox
+              <Textbox
                 text="Password"
                 containerWidth="378px"
                 type="password"
                 value={password}
                 onChange={handlePasswordChange}
-                
                 isFocused={isPassFocused}
                 handleInputFocus={handleInputFocus}
                 handleInputBlur={handleInputBlur}
-                />
+              />
             </div>
+          </div>
 
-            
-            </div>
-
-            <div className={styles.continueButton}>
+          <div className={styles.continueButton}>
             <Button
-                children={"Continue >"}
-                variant="colorful-button"
-                containerWidth={"378px"}
+              children={"Continue >"}
+              variant="colorful-button"
+              containerWidth={"378px"}
+              onClick={handleSubmit}
             />
-            </div>
+          </div>
         </div>
 
         <div className={styles.rightFrame}>
-            <div className={styles.topCurve}>
+          <div className={styles.topCurve}>
             <img src={top} alt="Description" />
-            </div>
+          </div>
 
-            <div className={styles.signinTextContainer}>
+          <div className={styles.signinTextContainer}>
             <div className={styles.mainSigninText}>
-                Grow your community. Keep people engaged. Receive Rewards. Expand
-                your Network.
+              Grow your community. Keep people engaged. Receive Rewards. Expand
+              your Network.
             </div>
-            </div>
+          </div>
 
-            <div className={styles.bottomCurve}>
+          <div className={styles.bottomCurve}>
             <img src={bottom} alt="Description" />
-            </div>
+          </div>
         </div>
-        </div>
-        
+      </div>
     </div>
-    );
-    }
+  );
+}
 
 export default Login;
